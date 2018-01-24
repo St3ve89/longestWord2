a1e59417a11f137f1c9db6b75594e51e27347209
St3ve89 <istvan.acs89@gmail.com>
2018-01-24 20:59:13 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function findLongestWord(str) {
+    var longestWord = 0;
+    str = str.split(" ");
+    for (var i = 0; i < str.length; i++) {
+        if (str[i].length > longestWord) {
+            longestWord = str[i].length;
+        }
+    }
+    return longestWord;
+}
+console.log(
+    findLongestWord("The quick brown fox jumped over the lazy dog")
+);
